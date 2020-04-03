82bf86db5e4c02afb5d297e20204b98299d1f9fa
Alexander Treib <alextreib94@gmail.com>
2020-04-03 10:37:08 +0200

    my-votings page

--- own-template-parts/content-my-votings.php
+++ own-template-parts/content-my-votings.php
@@ -29,8 +29,10 @@
 	<div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
 
 		<div class="entry-content">
-
+				<p> Test</p>
 			<?php
+
+
 			if (is_search() || !is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
 				the_excerpt();
 			} else {

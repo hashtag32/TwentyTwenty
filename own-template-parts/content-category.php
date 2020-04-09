fa7f0ee26c5c5192f554ad40ced0ecc02ec5eb51
Alexander Treib <alextreib94@gmail.com>
2020-04-09 20:08:46 +0200

    minor changes in category

--- own-template-parts/content-category.php
+++ own-template-parts/content-category.php
@@ -14,12 +14,32 @@
 
 ?>
 
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+<div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
+
+<div class="entry-content">
+
+	<?php
+	// copy value from 
+	if (is_search() || !is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
+		the_excerpt();
+	} else {
+		the_content(__('Continue reading', 'twentytwenty'));
+	}
+	?>
+
+</div><!-- .entry-content -->
+
+</div><!-- .post-inner -->
+
+<div class="section-inner">
+
 <?php $symbol=getSymbolName(single_cat_title( '', false ));
 if($symbol!="")
 {
 ?>
-
-<div class="post-inner">
 
 	
 	<!-- TradingView Widget BEGIN -->
@@ -85,6 +105,10 @@ if($symbol!="")
 	</script>
 	</div> -->
 
+	<!-- Prediction scale -->
+
+
+
 
 	<div class="stock-voting has-text-align-center ">
 		<!-- Third column = Your vote -->
@@ -102,12 +126,21 @@ if($symbol!="")
 		</form>
 	</div>
 
-</div><!-- .post-inner --> 
+	<div class="scoring-area">
+			<h2 class="has-text-align-center">Prediction Score </h2>
+			<h2 class="has-accent-color has-text-color has-text-align-center score-value">
+				<?php echo getPredictionScore($symbol)?> 
+				<span class="score-tooltip">
+					Score is a value between 0 and 5 with 0 = Selling and 5 = Buying
+				</span> 
+			</h2>  
+	</div> <!-- .scoring-area -->
+
+
 <?php 
 }
 ?>
-
-<div class="section-inner">
+ 
 
 </div><!-- .section-inner -->
 
@@ -118,19 +151,7 @@ if (is_single()) {
 	get_template_part('template-parts/navigation');
 }
 
-/**
- *  Output comments wrapper if it's a post, or if comments are open,
- * or if there's a comment number – and check for password.
- * */
-if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
 ?>
 
-	<div class="comments-wrapper section-inner">
-
-		<?php comments_template(); ?>
-
-	</div><!-- .comments-wrapper -->
-
-<?php
-}
-?>
+</article><!-- .post -->
+ 

9f66e5a2ab6946d34648a79ca33ae7c55ffa0896
Alexander Treib <alextreib94@gmail.com>
2020-05-08 14:26:16 +0200

    cleanup

--- templates/template-test.php
+++ templates/template-test.php
@@ -14,6 +14,53 @@ get_header();
 
 <main id="site-content" role="main">
 
+<?php
+$symbol = $wp_query->get( 'symbol' );
+echo $symbol;
+print_r($symbol);
+
+
+?>
+
+
+
+
+<div id="like_button_container"></div>
+
+<script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+
+  <!-- Load our React component. -->
+  <script>
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
+
+  </script>
+
+
 
 
 

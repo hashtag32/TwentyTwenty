367461319a18ae8db07938608818dbd75c7f5be6
Alexander Treib <alextreib94@gmail.com>
2020-05-08 14:26:07 +0200

    new functions for search optimization

--- php_functions/db_functions.php
+++ php_functions/db_functions.php
@@ -7,6 +7,58 @@ function InsertDataToDB($tableName,$parameterArray)
 	return $result;
 } 
 
+
+// Returns 
+function queryResultToArray($sqlQueryResult, $rowName=false)
+{
+	// $symbol=strtoupper($symbol);
+	$element_array=array();
+	if (mysqli_num_rows($sqlQueryResult) > 0) {
+		// output data of each row
+		while ($row = mysqli_fetch_assoc($sqlQueryResult)) {
+			// echo "id: " . $row["symbol"]. " - Name: " . $row["current_price"]. " " . $row["lastname"]. "<br>";
+            if($rowName!=false)
+            {
+				array_push($element_array,$row[$rowName]); 
+            }
+            else
+            {
+                array_push($element_array,$row); 
+            }
+        }
+	} else {
+		// echo "0 results";
+	}
+
+    return $element_array;
+}
+
+function search_strDB($tableName,$columns_array,$search_string)
+{
+    $sqlString = "SELECT * FROM " . $tableName . " WHERE ";
+
+    foreach($columns_array as $column)
+    {
+        $sqlString=$sqlString . $column . " LIKE '%" . $search_string . "%' ";
+        if(end($columns_array)!=$column)
+        {
+            $sqlString=$sqlString . " OR ";
+        }
+    }
+
+    $sqlResult=executeSQLCommand($sqlString);
+    $string_array=queryResultToArray($sqlResult);
+
+    return $string_array;
+}
+
+function executeSQLCommand($sqlString)
+{
+    $conn = connectDB();
+	$result = $conn->query($sqlString);
+	return $result;
+}
+
 function construct_sqlArray($tableName, $parameterArray)
 {
     $sqlString = "INSERT INTO " . $tableName;

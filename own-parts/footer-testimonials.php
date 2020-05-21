fe34ec8382c290fbda157f85fa741c814348ce39
Alexander Treib <alextreib94@gmail.com>
2020-05-21 23:39:25 +0200

    zoom for profile pictures in testimonials

--- own-parts/footer-testimonials.php
+++ own-parts/footer-testimonials.php
@@ -25,17 +25,17 @@
         <!-- Wrapper for carousel items -->
         <div class="carousel-inner">
           <div class="item carousel-item active">
-            <div class="img-box"><img src="https://stockvoting.net/wp-content/uploads/2020/04/max.jpg" alt=""></div>
+            <div class="img-box zoom"><img src="https://stockvoting.net/wp-content/uploads/2020/04/max.jpg" alt=""></div>
             <p class="testimonial">Through the analysis and ratings I have increased my performance hugely. Thanks to the StockVoting Team for such great support.</p>
             <p class="overview"><b>Maximilian Feld</b>, Financial Analyst</p>
           </div>
           <div class="item carousel-item">
-            <div class="img-box"><img src="https://stockvoting.net/wp-content/uploads/2020/05/antony_matts.png" alt=""></div>
+            <div class="img-box zoom"><img src="https://stockvoting.net/wp-content/uploads/2020/05/antony_matts.png" alt=""></div>
             <p class="testimonial">The idea of creating certificates and saving all the fees is disrupting.</p>
             <p class="overview"><b>Antony Matts</b>, Blockchain Expert</p>
           </div>
           <div class="item carousel-item">
-            <div class="img-box"><img src="https://stockvoting.net/wp-content/uploads/2020/05/silivia_miller.jpg" alt=""></div>
+            <div class="img-box zoom"><img src="https://stockvoting.net/wp-content/uploads/2020/05/silivia_miller.jpg" alt=""></div>
             <p class="testimonial">I was searching for such a platform for a long time. Please stay honest!</p>
             <p class="overview"><b>Silvia Miller</b>, Financial Journalist</p>
           </div>
